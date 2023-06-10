ac67a0a5bcbce2ebee95cb32be129a0fae2365c9
Hamseapdiasiis <hamse906@gmail.com>
2023-06-10 08:21:31 +0300

    Exercise done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+
+function findSeven(values){
+
+    
+
+    for(let i = 0; i < values.length; i++){
+        if(values[i] == 7){
+            console.log("number 7 Found")
+        }if (values[i] != 7){
+            console.log("there is no number 7")
+        }
+    }
+}
+
+const numbers = [1,2,3,4,9,0]
+findSeven(numbers)
